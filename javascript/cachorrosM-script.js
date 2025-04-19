ed5bbabc097fbd7d70df4a7ca781516ecae73536
tenluvid <annapachevo890@gmail.com>
2025-04-19 16:49:56 -0300

    Fix: JavaScript funcionado

--- javascript/cachorrosM-script.js
+++ javascript/cachorrosM-script.js
@@ -1,8 +1,9 @@
-function AbrirModal(nome, idade, status, historia, imagem) {
+function AbrirModal(nome, idade, status, historia, modo, imagem) {
     document.getElementById('modal-nome').textContent = nome;
     document.getElementById('modal-idade').textContent = idade;
     document.getElementById('modal-status').textContent = status;
     document.getElementById('modal-historia').textContent = historia;
+    document.getElementById('modal-modo').textContent = modo;
     document.getElementById('modal-img').src = imagem;
     document.getElementById('modal-img').alt = nome;
 
