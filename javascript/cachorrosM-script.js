e66ea41e8924c709d2e14e0922d3b7e920e5405e
tenluvid <annapachevo890@gmail.com>
2025-04-19 15:58:06 -0300

    Fix: testando javascript
    Colocando js no siete "Cachorros Machos" - para colocar as informações de cada um

---
+++ javascript/cachorrosM-script.js
@@ -0,0 +1,14 @@
+function AbrirModal(nome, idade, status, historia, imagem) {
+    document.getElementById('modal-nome').textContent = nome;
+    document.getElementById('modal-idade').textContent = idade;
+    document.getElementById('modal-status').textContent = status;
+    document.getElementById('modal-historia').textContent = historia;
+    document.getElementById('modal-img').src = imagem;
+    document.getElementById('modal-img').alt = nome;
+
+    document.getElementById('modal').style.display = 'flex';
+}
+
+function fecharModal() {
+    document.getElementById('modal').style.display = 'none';
+}
